2f3e84aefa1846a9ac63bb332203752f9ebc315d
John Doe <revans@14west.us>
2019-11-26 12:30:17 -0500

    ITDEVOPS-2200 perm issues still

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if ! whoami &> /dev/null; then
+  echo 'Setting up etc/passwd for artibtrary user'
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 if ! [ -f backup-cron ]
 then
   echo "Creating go-crond entry to start backup at: $BACKUP_TIME"
